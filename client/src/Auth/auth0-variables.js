b5056a147ab61963cf2b88ec377dbc9db75c3903
Whitney Wong <whitney.wong@utexas.edu>
2017-10-04 18:01:38 -0500

    updated link

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -2,5 +2,6 @@ export const AUTH_CONFIG = {
   domain: 'ruigrok.auth0.com',
   clientId: 'tzGoycNCrlv5n1v5N227cN61JfCD4El4',
   //callbackUrl: 'http://localhost:3000/callback'
-  callbackUrl: 'https://dewey-book-club.herokuapp.com/callback'
+  // callbackUrl: 'https://dewey-book-club.herokuapp.com/callback'
+  callbackUrl: 'https://shielded-taiga-73403.herokuapp.com/callback'
 }
